58627087d6bd9d0d5af9af576c9f1bb6a42b8484
Luke <mythnan@gmail.com>
2020-10-11 23:06:12 +1100

    Implement Authentication feature

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UsePipes,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
 import { ValidationPipe } from 'src/shared/validation.pipe';
+import { AuthGuard } from 'src/shared/auth.guard';
+import { User } from './user.decorator';
+import { UserEntity } from './user.entity';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/user')
-  showAllUsers() {
+  @UseGuards(new AuthGuard())
+  showAllUsers(@User() user: UserEntity) {
+    console.log(user);
     return this.userService.showAll();
   }
 
